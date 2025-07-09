bfffce3bbc49215e60397c06ba24188e8a999573
Tomer Levy <tomerlv104@gmail.com>
2025-07-09 09:46:47 +0300

    Comments

--- src/components/SavedTripsList.js
+++ src/components/SavedTripsList.js
@@ -2,6 +2,14 @@
 import React from "react";
 import TripCard from "./TripCard";
 
+/**
+ * Component to display a list of saved trips.
+ * @param {Object[]} trips - Array of trip objects to display.
+ * @param {Function} onDelete - Callback function to handle trip deletion.
+ * @param {Function} onClick - Callback function to handle trip card click.
+ * @param {number} [limit] - Optional limit on the number of trips to display.
+ * @return {JSX.Element} Rendered list of trip cards or a message if no trips are found.
+ */
 function SavedTripsList({ trips, onDelete, onClick, limit }) {
   const tripsToShow = limit ? trips.slice(0, limit) : trips;
 
